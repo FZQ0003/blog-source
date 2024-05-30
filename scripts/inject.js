b6ea151d2fce991837615f1019f67ed5e0ec5634
FZQ0003 <468403876@qq.com>
2024-05-30 20:58:58 +0800

    Change comment plugin

--- scripts/inject.js
+++ scripts/inject.js
@@ -3,9 +3,9 @@ hexo.extend.filter.register('theme_inject', (injects) => {
     // Body
     injects.bodyEnd.file('check_url', 'source/_inject/check_url.ejs');
     // Comments
-    injects.postComments.file('default', 'source/_inject/gitalk.ejs');
-    injects.pageComments.file('default', 'source/_inject/gitalk.ejs');
-    injects.linksComments.file('default', 'source/_inject/gitalk.ejs');
+    injects.postComments.file('default', 'source/_inject/comments.ejs');
+    injects.pageComments.file('default', 'source/_inject/comments.ejs');
+    injects.linksComments.file('default', 'source/_inject/comments.ejs');
     // Copyright
     injects.postCopyright.file('default', 'source/_inject/copyright.ejs');
 });

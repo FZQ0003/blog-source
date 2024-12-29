98057963f25b44f15c967e549a4556320b2fca46
FZQ0003 <468403876@qq.com>
2024-12-30 01:22:34 +0800

    Update scripts

--- pre_commit.sh
+++ pre_commit.sh
@@ -6,25 +6,50 @@ if [ -f "$1" ]; then
     set +a
 fi
 
-if [ -z "$OUTPUT_PREFIX" ]; then
-    OUTPUT_PREFIX=posts_ex
-fi
-
 if [ -n "$ENCRYPT_KEY" ]; then
-    cd "source/_posts_enc"
-    echo "Compressing special posts..."
-    tar -Jcvf "$OUTPUT_PREFIX.tar.xz" *.md */
-    echo "Checking if update is needed..."
-    sha256sum -c "$OUTPUT_PREFIX.tar.xz.sha256" 2> /dev/null
-    if [ $? != 0 ]; then
-        echo "Updating encrypted archive..."
-        sha256sum "$OUTPUT_PREFIX.tar.xz" > "$OUTPUT_PREFIX.tar.xz.sha256"
-        printf "$ENCRYPT_KEY" | tr -d '[\r\n]' | base64 -d | openssl enc \
-            -aes-256-cbc -iter 256 -pbkdf2 \
-            -in "$OUTPUT_PREFIX.tar.xz" -out "$OUTPUT_PREFIX.tar.xz.enc" \
-            -pass stdin
+    cd "source_extra"
+    echo "Compressing extra sources..."
+    # Create a list of files to compress
+    if [[ ! -f "list.txt" ]]; then
+        echo "source_extra *.md *.html */" > "list.txt"
     fi
-    cd "../.."
+    # Update the archives
+    while IFS=' ' read -r archive_name file_paths; do
+        echo "Updating $archive_name..."
+        if [[ -z "$file_paths" ]]; then
+            echo " -> Skipped"
+            continue
+        fi
+        # Parse the file paths
+        files_to_compress=""
+        for file_path in $file_paths; do
+            if [[ -e "$file_path" ]]; then
+                files_to_compress="$files_to_compress $file_path"
+            fi
+        done
+        if [[ -z "$files_to_compress" ]]; then
+            echo " -> Skipped"
+            continue
+        fi
+        # Compress the files
+        tar -Jcvf "$archive_name.tar.xz" $files_to_compress
+        if [[ $? != 0 ]]; then
+            echo " -> Error"
+            continue
+        fi
+        # Check & update the encrypted archive
+        echo "Checking if update is needed..."
+        sha256sum -c "$archive_name.tar.xz.sha256" 2> /dev/null
+        if [[ $? != 0 ]]; then
+            echo "Updating encrypted archive..."
+            sha256sum "$archive_name.tar.xz" > "$archive_name.tar.xz.sha256"
+            printf "$ENCRYPT_KEY" | tr -d '[\r\n]' | base64 -d | openssl enc \
+                -aes-256-cbc -iter 256 -pbkdf2 \
+                -in "$archive_name.tar.xz" -out "$archive_name.tar.xz.enc" \
+                -pass stdin
+        fi
+    done < list.txt
+    cd ".."
 fi
 
 echo "Done!"
--- pre_deploy.sh
+++ pre_deploy.sh
@@ -6,33 +6,31 @@ if [ -f "$1" ]; then
     set +a
 fi
 
-if [ -z "$OUTPUT_PREFIX" ]; then
-    OUTPUT_PREFIX=posts_ex
-fi
-
 if [ -n "$ENCRYPT_TAGS_LIST" ]; then
     echo "Applying changes for config.encrypt.tags..."
 	sed -i 's/#\s*\(\S*\)\s*\(secrets.ENCRYPT_TAGS_LIST\)/\1 '"$ENCRYPT_TAGS_LIST"'/g' _config.yml
 fi
 
 if [ -n "$ENCRYPT_KEY" ]; then
-    cd "source/_posts_enc"
-    if [ -f "$OUTPUT_PREFIX.tar.xz.enc" ]; then
-        echo "Decrypting archive..."
+    cd "source_extra"
+    find . -type f -name "*.tar.xz.enc" | while read -r archive; do
+        prefix="${archive%%.tar.xz.enc}"
+        echo "Decrypting $(basename $prefix)..."
+        mv "$prefix.tar.xz" "$prefix.tar.xz.bak"
         printf "$ENCRYPT_KEY" | tr -d '[\r\n]' | base64 -d | openssl enc -d \
                 -aes-256-cbc -iter 256 \
-                -in "$OUTPUT_PREFIX.tar.xz.enc" -out "$OUTPUT_PREFIX.tar.xz" \
+                -in "$prefix.tar.xz.enc" -out "$prefix.tar.xz" \
                 -pass stdin
         echo "Checking..."
-        sha256sum -c "$OUTPUT_PREFIX.tar.xz.sha256" 2> /dev/null
-        if [ $? == 0 ]; then
-            echo "Uncompressing special posts..."
-            tar -Jxvf "$OUTPUT_PREFIX.tar.xz" -C ../_posts
-        else
+        sha256sum -c "$prefix.tar.xz.sha256" 2> /dev/null
+        if [ $? != 0 ]; then
             echo "Warning: Check failed!"
+            continue
         fi
-    fi
-    cd "../.."
+        echo "Uncompressing special posts..."
+        tar -Jxvf "$prefix.tar.xz" -C "../source"
+    done
+    cd ".."
 fi
 
 echo "Done!"
